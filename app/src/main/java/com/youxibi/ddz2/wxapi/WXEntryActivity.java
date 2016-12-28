2f6edc231d6c186671661abb21a5795d5f6056ba
heyuanchao <1205386762@qq.com>
2016-12-28 17:03:03 +0800

    提交

--- app/src/main/java/com/youxibi/ddz2/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/youxibi/ddz2/wxapi/WXEntryActivity.java
@@ -2,9 +2,9 @@ package com.youxibi.ddz2.wxapi;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.tencent.mm.sdk.modelbase.BaseReq;
 import com.tencent.mm.sdk.modelbase.BaseResp;
@@ -15,8 +15,17 @@ import com.tencent.mm.sdk.openapi.WXAPIFactory;
 import com.youxibi.ddz2.MainActivity;
 import com.youxibi.ddz2.R;
 
-public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+    private static final String TAG = "WXEntryActivity";
     // IWXAPI 是第三方app和微信通信的openapi接口
     private IWXAPI api;
 
@@ -54,7 +63,7 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
                     String code = ((SendAuth.Resp) resp).code;
                     String state = ((SendAuth.Resp) resp).state;
                     Log.i("@@@", "code: " + code + ", state: " + state);
-                    // getToken(code, state);
+                    getToken(code, state);
                 }
                 break;
             case BaseResp.ErrCode.ERR_USER_CANCEL:
@@ -66,5 +75,83 @@ public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
         }
 
         finish();
+    }
+
+    private void getToken(String code, String state) {
+        new DownloadTask().execute("http://www.baidu.com");
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getString(R.string.connection_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "result: " + result);
+        }
+    }
+
+    private String loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        String str ="";
+
+        try {
+            stream = downloadUrl(urlString);
+            // str = readIt(stream, 500);
+            str = read(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+
+    private InputStream downloadUrl(String urlString) throws IOException {
+        // BEGIN_INCLUDE(get_inputstream)
+        Log.i(TAG, "url: " + urlString);
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        // conn.connect();
+        Log.i(TAG, "response code: " + conn.getResponseCode());
+        if (conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM || conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
+            // Log.i(TAG, conn.getHeaderFields().toString());
+            return downloadUrl(conn.getHeaderField("Location"));
+        }
+        InputStream stream = conn.getInputStream();
+        return stream;
+        // END_INCLUDE(get_inputstream)
+    }
+
+    private String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    //从流中读取数据
+    private String read(InputStream inStream) throws IOException{
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        return new String(outStream.toByteArray());
     }
 }
