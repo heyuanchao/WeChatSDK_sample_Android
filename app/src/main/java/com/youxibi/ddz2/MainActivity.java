cc2dc4d051815b8f4299822a3cd3bf6ddfad143d
heyuanchao <1205386762@qq.com>
2017-01-26 00:58:03 +0800

    提交

--- app/src/main/java/com/youxibi/ddz2/MainActivity.java
+++ app/src/main/java/com/youxibi/ddz2/MainActivity.java
@@ -1,20 +1,35 @@
 package com.youxibi.ddz2;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
 
     // APP_ID 替换为你的应用从官方网站申请到的合法appId
     public static final String APP_ID = "wx724c4dda97632881";
     public static final String APP_SECRECT = "c566e685587ca7ba01f579e909f817c1";
 
     private IWXAPI api;
+
+    private String params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +59,122 @@ public class MainActivity extends Activity {
         findViewById(R.id.pay_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                new GetParameterTask().execute("http://www.mojing.com/2.php");
+
+//                try {
+//                    Util.httpPost("https://api.mch.weixin.qq.com/pay/unifiedorder", params);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
 
             }
         });
+    }
+
+    private class GetParameterTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return Util.httpGet(urls[0]);
+            } catch (IOException e) {
+                return getString(R.string.connection_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "result: " + result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+
+                String appid = jsonObject.optString("appid");
+                String body = jsonObject.optString("body");
+                String key = jsonObject.optString("key");
+                String mch_id = jsonObject.optString("mch_id");
+                String nonce_str = Util.getRandomString(1, 32);
+//                String nonce_str = "5K8264ILTKCH16CQ2502SI8ZNMTM67VS";
+                String notify_url = jsonObject.optString("notify_url");
+
+                /*
+                Map<String, String> p = new HashMap<>();
+                p.put("appid", jsonObject.optString("appid"));
+                p.put("body", jsonObject.optString("body"));
+                p.put("key", jsonObject.optString("key"));
+                p.put("mch_id", jsonObject.optString("mch_id"));
+                p.put("nonce_str", Util.getRandomString(32));
+                */
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd", Locale.CHINA);
+//                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss", Locale.CHINA);
+                String out_trade_no = formatter.format(new Date()) + Util.getRandomString(0, 6);
+//                String out_trade_no = formatter.format(new Date());
+//                String out_trade_no = "20150806125346";
+//                p.put("out_trade_no", out_trade_no);
+
+                // p.put("spbill_create_ip", jsonObject.optString("spbill_create_ip"));
+                String spbill_create_ip = jsonObject.optString("spbill_create_ip");
+                int total_fee = 1;
+                // p.put("total_fee", "1");
+
+                String stringSignTemp = "appid=" + appid
+                        + "&body=" + body
+                        + "&mch_id=" + mch_id
+                        + "&nonce_str=" + nonce_str
+                        + "&notify_url=" + notify_url
+                        + "&out_trade_no=" + out_trade_no
+                        + "&spbill_create_ip=" + spbill_create_ip
+                        + "&total_fee=" + total_fee
+                        + "&trade_type=APP"
+                        + "&key=" + key;
+                String sign = Util.MD5(stringSignTemp).toUpperCase();
+                // p.put("sign", sign);
+
+                params = "<xml>" + "<appid>" + appid + "</appid>"
+                        + "<body><![CDATA[" + body + "]]></body>"
+                        + "<mch_id>" + mch_id + "</mch_id>"
+                        + "<nonce_str>" + nonce_str + "</nonce_str>"
+                        + "<notify_url>" + notify_url + "</notify_url>"
+                        + "<out_trade_no>" + out_trade_no + "</out_trade_no>"
+                        + "<spbill_create_ip>" + spbill_create_ip + "</spbill_create_ip>"
+                        + "<total_fee>" + total_fee + "</total_fee>"
+                        + "<trade_type>APP</trade_type>"
+                        + "<sign>" + sign + "</sign>"
+                        + "</xml>";
+
+                Log.i(TAG, params);
+
+                new GetPrepayIdTask().execute("https://api.mch.weixin.qq.com/pay/unifiedorder");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class GetPrepayIdTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return Util.httpPost(urls[0], params);
+            } catch (IOException e) {
+                return getString(R.string.connection_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(TAG, "result: " + result);
+
+            Map<String, String> map = Util.parseXML(result);
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                Log.i(TAG, entry.getKey() + ": " + entry.getValue());
+            }
+
+            if (map.get("prepay_id") != null) {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/youxibi/ddz2/Util.java
+++ app/src/main/java/com/youxibi/ddz2/Util.java
@@ -1,13 +1,29 @@
 package com.youxibi.ddz2;
 
+import android.content.res.XmlResourceParser;
+import android.util.Xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+
+import static android.R.id.list;
 
 public class Util {
 
@@ -17,7 +33,6 @@ public class Util {
 
         try {
             stream = downloadUrl(urlString, "GET", null);
-            // str = readIt(stream, 500);
             str = read(stream);
         } finally {
             if (stream != null) {
@@ -27,13 +42,29 @@ public class Util {
         return str;
     }
 
+    /*
     public static String httpPost(String urlString, Map<String, String> params) throws IOException {
         InputStream stream = null;
         String str ="";
 
         try {
             stream = downloadUrl(urlString, "POST", params);
-            // str = readIt(stream, 500);
+            str = read(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+    */
+
+    public static String httpPost(String urlString, String params) throws IOException {
+        InputStream stream = null;
+        String str ="";
+
+        try {
+            stream = downloadUrl(urlString, "POST", params);
             str = read(stream);
         } finally {
             if (stream != null) {
@@ -43,19 +74,37 @@ public class Util {
         return str;
     }
 
+    /*
     private static InputStream downloadUrl(String urlString, String method, Map<String, String> params) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod(method);
+
+        if (params != null) {
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(getRequestData(params, "UTF-8").getBytes());
+        }
+
+        if (conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM || conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
+            return downloadUrl(conn.getHeaderField("Location"), "POST", params);
+        }
+        InputStream stream = conn.getInputStream();
+        return stream;
+    }
+    */
+
+    private static InputStream downloadUrl(String urlString, String method, String params) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setReadTimeout(10000 /* milliseconds */);
         conn.setConnectTimeout(15000 /* milliseconds */);
         conn.setRequestMethod(method);
-//        conn.setDoInput(true);
-        // Start the query
-        // conn.connect();
 
         if (params != null) {
             OutputStream outputStream = conn.getOutputStream();
-            outputStream.write(getRequestData(params, "UTF-8").getBytes());
+            outputStream.write(params.getBytes());
         }
 
         if (conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM || conn.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
@@ -88,11 +137,90 @@ public class Util {
     public static String read(InputStream inStream) throws IOException{
         ByteArrayOutputStream outStream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
-        int len = 0;
+        int len;
         while((len = inStream.read(buffer)) != -1) {
             outStream.write(buffer, 0, len);
         }
         inStream.close();
         return new String(outStream.toByteArray());
     }
+
+    public static String getRandomString(int type, int length) { //length表示生成字符串的长度
+        String base = "0123456789";
+        if (type == 1) {
+//            base = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789";
+            base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        }
+
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static String MD5(String s) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(s.getBytes("UTF-8"));
+            byte[] encryption = md5.digest();
+
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < encryption.length; i++) {
+                if (Integer.toHexString(0xff & encryption[i]).length() == 1) {
+                    sb.append("0").append(Integer.toHexString(0xff & encryption[i]));
+                } else {
+                    sb.append(Integer.toHexString(0xff & encryption[i]));
+                }
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    /**
+     * @description 将xml字符串转换成map
+     * @param xml
+     * @return Map
+     */
+    public static Map<String, String> parseXML(String xml) {
+        Map<String, String> map = new HashMap<>();
+
+        ByteArrayInputStream stream = new ByteArrayInputStream(xml.getBytes());
+        XmlPullParser parser = Xml.newPullParser();
+        try {
+            parser.setInput(stream, "UTF-8");
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = parser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:// 开始元素事件
+                        if ("appid".equals(nodeName) || "nonce_str".equals(nodeName) || "mch_id".equals(nodeName) || "prepay_id".equals(nodeName) || "sign".equals(nodeName)) {
+                            map.put(nodeName, parser.nextText());
+                        }
+
+                        break;
+                    case XmlPullParser.END_TAG:// 结束元素事件
+                        break;
+                }
+                eventType = parser.next();
+            }
+            stream.close();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
+    }
+
 }
